a0c3891e188e9c59554f91917880d728030c7248
Chris <codemedarkone@gmail.com>
2017-09-17 18:26:13 -0600

    added cards to view via code facing back and tested object observers

--- Match/Card.swift
+++ Match/Card.swift
@@ -11,17 +11,33 @@ import UIKit
 class Card: UIView {
 
     let cardImageView = UIImageView()
-    var cardValue = 0
+    var cardValue = 0 
     let cardNames = ["card2", "card3", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         // custom functionality
-        //TODO: Initialize the imageview with an image
-        //TODo: add it to the view
-        //TODO: set constraints for the imageview
         
+        // add image view into view
+        addSubview(cardImageView)
+        
+        //TODO: Initialize the imageview with an image
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "back")
+        
+        //TODO: set constraints for the imageview
+        let heightConstraint = NSLayoutConstraint(item: cardImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 170)
+        
+        let widthConstraint = NSLayoutConstraint(item: cardImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+        
+        cardImageView.addConstraints([widthConstraint, heightConstraint])
+        
+        let topConstraint = NSLayoutConstraint(item: cardImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0)
+        
+        let leftConstraint = NSLayoutConstraint(item: cardImageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0)
+        
+        addConstraints([topConstraint, leftConstraint])
     }
     
     required init?(coder aDecoder: NSCoder) {
--- Match/ViewController.swift
+++ Match/ViewController.swift
@@ -27,7 +27,47 @@ class ViewController: UIViewController {
         //put the 4 stackViews into an array
         stackViewArray += [firstStackView, secondStackView, thirdStackView, fourthStackView]
         
+        //Get the cards
         cards = model.getCards()
+        
+        // Layout the cards
+        layoutcards()
+    }
+    
+    func layoutcards() {
+        
+        //keeps track of which card we're at
+        var cardIndex = 0
+        
+        // loop through the four stackviews and put four crads into each
+        for stackview in stackViewArray {
+            
+            // put four card objects into the stackview
+            for _ in 1...4{
+                
+                // check if card exists at index before adding
+                if cardIndex < cards.count {
+                    
+                    // Set card that we're looking at
+                    let card = cards[cardIndex]
+                    card.translatesAutoresizingMaskIntoConstraints = false
+                    
+                    // Set the size of the card object
+                    let heightConstraint = NSLayoutConstraint(item: card, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 170)
+                    
+                    let widthContraint = NSLayoutConstraint(item: card, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+                    
+                    card.addConstraints([heightConstraint, widthContraint])
+                    
+//                    //set a color
+//                    card.backgroundColor = UIColor.red
+                
+                    //put a card into the stackView
+                    stackview.addArrangedSubview(cards[cardIndex])
+                    cardIndex += 1
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
